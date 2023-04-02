67d78503d38742e6615c3a4a1f7fcad1b8fd9a5c
ekoka <66888386+eekoka@users.noreply.github.com>
2023-04-02 22:20:37 +0200

    Adds div placeholder, DOM modifications and styles
    Adds DOM modifications to show running results and overall results.
    Adds styles in CSS; assigns colors for win, loss, draw.

--- script.js
+++ script.js
@@ -30,107 +30,86 @@ function playRound(playerSelection, computerSelection) {
    }
 }
 
-function game (totalRounds=5) {
+//Plays 5 rounds - the number of rounds can be added as parameter
+//then play can choose number of rounds they want to play
+function game () {
 
-   console.log(totalRounds + "-round Game Not Counting Draws.");
+   //get reference to the container that already exist in the html
+   const playerDiv = document.querySelector("#player-div");
+   const computerDiv = document.querySelector("#computer-div");
+   const resultDiv1 = document.querySelector("#result-line1");
+   const resultDiv2 = document.querySelector("#result-line2");
+   const resultDiv3 = document.querySelector("#result-line3");
 
+   //initialize variables
    let playerScore = 0;
    let computerScore = 0;
-   //const totalRounds = 5;
-   let playerVerdict;
+   const totalRounds = 5;
    let r = 1;
 
-   //update code at a later stage to allow player to choose total number of rounds to play
-   while (r <= totalRounds) {
+   //buttons is a node list - iterate through each button
+   //and launch round when any of the button is clicked
+   const buttons = document.querySelectorAll(".choice-button");
+   buttons.forEach((button)=> {
+   button.addEventListener("click", (e) => {
+         let playerVerdict;
+         let currentRound = r;
 
-     console.log("Round " + r);
+         //do not run of total number of rounds achieved
+         if (r > totalRounds) return;
+      
+         const playerSelection = e.target.id; 
+         const computerSelection = getComputerChoice();
 
-     //make a function to get player choice and validate of entry if typed in
-     const playerSelection = playerInput;
-     const computerSelection = getComputerChoice();
+         playerVerdict = playRound(playerSelection, computerSelection);
+         
+         playerDiv.textContent = "Your Choice: " + playerSelection;
+         computerDiv.textContent = "Computer Choice: " + computerSelection;
 
-     playerVerdict = playRound(playerSelection, computerSelection);
-     console.log("You " + playerVerdict + " Round");
-     
-     if (playerVerdict == "Win") {
-       playerScore++;
-       r++;
-     } else if (playerVerdict == "Loss") {
-       computerScore++;
-       r++;
-     }
-   }
+         if (playerVerdict == "Win") {
+            resultDiv1.style.color = 'green';
+            playerScore++;
+            r++;
+         } else if (playerVerdict == "Loss") {
+            resultDiv1.style.color = 'red';
+            computerScore++;
+            r++;
+         } else {
+            resultDiv1.style.color = 'black';
+         }
 
-   console.log("Game Over!");
-   if (playerScore > computerScore) console.log("You Win Overall!");
-   else if (playerScore == computerScore) console.log("Draw Game Overall!");
-   else console.log("You Loss Overall!");
-   console.log("Your Final Score: "  + playerScore + " / " + totalRounds);
-   if (playerScore == totalRounds) console.log("PERFECT SCORE!!!");
-   if (playerScore == 0) console.log("YOU GOT WRECKED!!!");
+         //add some text in the new div
+         resultDiv1.textContent = "You " + playerVerdict + " Round " + currentRound;
+         resultDiv2.textContent = "Continue Playing";
+
+         //show results when game is over
+         let finalResult = "";
+         if (r === totalRounds+1) {
+            //removing choice buttons at the end of game
+            const choiceContainer = document.querySelector(".choice-container");
+            choiceContainer.parentNode.removeChild(choiceContainer);
+            const subInstruction = document.querySelector("#sub-instruction");
+            subInstruction.parentNode.removeChild(subInstruction);
+
+            if (playerScore > computerScore) {
+               finalResult = "You Win Overall!";
+               resultDiv2.style.color = 'green';
+            } else if (playerScore == computerScore) {
+               finalResult = "Draw Game Overall!";
+            } else {
+               finalResult = "You Loss Overall!";
+               resultDiv2.style.color = 'red';
+            } 
+            resultDiv2.textContent = "Game Over: " + finalResult;
+            resultDiv3.textContent = "Your Final Score is "  + playerScore + " / " + totalRounds;
+
+            if (playerScore == totalRounds) resultDiv3.textContent += ", PERFECT SCORE!";
+            if (playerScore == 0) resultDiv3.textContent += ", YOU GOT WRECKED!";
+         }
+   });
+   });
+
 }
 
-//game();
-
-//get reference to the container that already exist in the html
-const container = document.querySelector("#information");
-
-//DOM modifications to display results
-const subContainer = document.createElement("div");
-subContainer.classList.add("sub-container");
-const playerDiv = document.createElement("div");
-playerDiv.classList.add("player-div");
-const computerDiv = document.createElement("div");
-computerDiv.classList.add("computer-div");
-subContainer.appendChild(playerDiv);
-subContainer.appendChild(computerDiv);
-container.appendChild(subContainer);
-
-const subContainer2 = document.createElement("div");
-subContainer2.classList.add("result2-sub-container");
-const resultDiv1 = document.createElement("p");
-resultDiv1.classList.add("result-line1");
-const resultDiv2 = document.createElement("p");
-resultDiv2.classList.add("result-line2");
-//append the div to container
-subContainer2.appendChild(resultDiv1);
-subContainer2.appendChild(resultDiv2);
-container.appendChild(subContainer2);
-
-//initialize variables
-let playerScore = 0;
-let computerScore = 0;
-const totalRounds = 5;
-let r = 1;
-
-//buttons is a node list - iterate through each button
-//and launch round when any of the button is clicked
-const buttons = document.querySelectorAll(".choice-button");
-buttons.forEach((button)=> {
-  button.addEventListener("click", (e) => {
-      let playerVerdict;
-
-      //do not run of total number of rounds achieved
-      if (r > totalRounds) return;
- 
-      const playerSelection = e.target.id; 
-      const computerSelection = getComputerChoice();
- 
-      playerVerdict = playRound(playerSelection, computerSelection);
-      
-      playerDiv.textContent = "You Choice: " + playerSelection;
-      computerDiv.textContent = "Computer Choice: " + computerSelection;
-
-      //add some text in the new div
-      resultDiv1.textContent = "You " + playerVerdict + " Round " + r;
-      
-      if (playerVerdict == "Win") {
-        playerScore++;
-        r++;
-      } else if (playerVerdict == "Loss") {
-        computerScore++;
-        r++;
-      }
-      resultDiv2.textContent = "Play Round " + r;
-  });
-});
+//run game
+game();
