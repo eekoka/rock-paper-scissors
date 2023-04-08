680f5952c156959d64a57786e1969d96343823fe
ekoka <66888386+eekoka@users.noreply.github.com>
2023-04-08 02:15:13 +0200

    Add images to game and updated game
    Add images to game.
    Updated game to first to reach 5 points instead of playing 5 rounds.

--- script.js
+++ script.js
@@ -35,13 +35,20 @@ function playRound(playerSelection, computerSelection) {
    }
 }
 
-//Plays 5 rounds - the number of rounds can be added as parameter
-//then play can choose number of rounds they want to play
+//Play for first player to reach 5 points: target score
+//the target score can be added as parameter
+//then player can choose target score before start of game
 function game () {
 
    //get reference to the container that already exist in the html
    const playerDiv = document.querySelector("#player-div");
    const computerDiv = document.querySelector("#computer-div");
+   const playerDivText = document.querySelector("#player-div-text");
+   const computerDivText = document.querySelector("#computer-div-text");
+
+   const playerImg = document.createElement("img");
+   const computerImg = document.createElement("img");
+
    const resultDiv1 = document.querySelector("#result-line1");
    const resultDiv2 = document.querySelector("#result-line2");
    const resultDiv3 = document.querySelector("#result-line3");
@@ -49,52 +56,67 @@ function game () {
    //initialize variables
    let playerScore = 0;
    let computerScore = 0;
-   const totalRounds = 5;
+   const targetScore = 5;
    let r = 1;
+   let gameOver = false;
+   let playerVerdict;
+   let finalResult;
+   let currentRound;
 
    //buttons is a node list - iterate through each button
    //and launch round when any of the button is clicked
    const buttons = document.querySelectorAll(".choice-button");
    buttons.forEach((button)=> {
    button.addEventListener("click", (e) => {
-         let playerVerdict;
-         let currentRound = r;
 
+         currentRound = r;
          //do not run of total number of rounds achieved
-         if (r > totalRounds) return;
+         if (gameOver === true) return;
       
          const playerSelection = getPlayerChoice(e.target.id); 
          const computerSelection = getComputerChoice();
 
          playerVerdict = playRound(playerSelection, computerSelection);
          
-         playerDiv.textContent = "Your Choice: " + playerSelection;
-         computerDiv.textContent = "Computer Choice: " + computerSelection;
+         playerDivText.textContent = "Your Choice: " + playerSelection;
+         computerDivText.textContent = "Computer Choice: " + computerSelection;
+
+         //show image of player choice and computer choice
+         playerImg.src = "images/player-" + playerSelection.toLowerCase() + ".png";
+         computerImg.src = "images/computer-" + computerSelection.toLowerCase() + ".png";
+         playerImg.width = 200;
+         computerImg.width = 200;
+         playerDiv.appendChild(playerImg);
+         computerDiv.appendChild(computerImg);    
 
          if (playerVerdict == "Win") {
             resultDiv1.style.color = 'green';
             playerScore++;
-            r++;
          } else if (playerVerdict == "Loss") {
             resultDiv1.style.color = 'red';
             computerScore++;
-            r++;
          } else {
             resultDiv1.style.color = 'black';
          }
 
-         //add some text in the new div
-         resultDiv1.textContent = "You " + playerVerdict + " Round " + currentRound;
-         resultDiv2.textContent = "Continue Playing";
+         r++;
 
-         //show results when game is over
-         let finalResult = "";
-         if (r === totalRounds+1) {
+         //Over ends when a player reaches 5 points
+         if (playerScore >= targetScore || computerScore >= targetScore) gameOver = true;
+
+         if (gameOver !== true) {
+            //add some text in the new div
+            resultDiv1.textContent = "You " + playerVerdict + " Round " + currentRound;
+            resultDiv2.textContent = playerScore + " - " + computerScore;
+            resultDiv3.textContent = "Continue Playing";
+         } else if (gameOver === true) {
+            //show results when game is over
             //removing choice buttons at the end of game
             const choiceContainer = document.querySelector(".choice-container");
             choiceContainer.parentNode.removeChild(choiceContainer);
             const subInstruction = document.querySelector("#sub-instruction");
             subInstruction.parentNode.removeChild(subInstruction);
+            resultDiv1.parentNode.removeChild(resultDiv1);
 
             if (playerScore > computerScore) {
                finalResult = "You Win Overall!";
@@ -105,12 +127,14 @@ function game () {
                finalResult = "You Loss Overall!";
                resultDiv2.style.color = 'red';
             } 
-            resultDiv2.textContent = "Game Over: " + finalResult;
-            resultDiv3.textContent = "Your Final Score is "  + playerScore + " / " + totalRounds;
 
-            if (playerScore == totalRounds) resultDiv3.textContent += ", PERFECT SCORE!";
+            resultDiv2.textContent = playerScore + " - " + computerScore;
+            resultDiv3.textContent = "Game Over: " + finalResult;
+
+            if (computerScore == 0) resultDiv3.textContent += ", PERFECT SCORE!";
             if (playerScore == 0) resultDiv3.textContent += ", YOU GOT WRECKED!";
          }
+
    });
    });
 
